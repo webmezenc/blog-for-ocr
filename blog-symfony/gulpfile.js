c0dce13ef91268cb790588b781765d439f46f8d8
webmezenc <contact@webmezenc.com>
2018-07-30 17:50:43 +0200

    Modification sur la template de base

--- blog-symfony/gulpfile.js
+++ blog-symfony/gulpfile.js
@@ -1,18 +1,38 @@
-var gulp = require('gulp');
-var merge = require('merge-stream');
-
+const gulp = require('gulp');
+const merge = require('merge-stream');
+const sass = require('gulp-sass');
+const gulpminify = require('gulp-minify');
 
 gulp.task('default', function() {
 
-    var bootstrap = gulp.src('node_modules/bootstrap/dist/css/*.css')
+    const bootstrap = gulp.src('node_modules/bootstrap/dist/css/*.css')
         .pipe( gulp.dest('public/assets/css/') );
 
-    var bootstrapjs = gulp.src('node_modules/bootstrap/dist/js/*.js')
+    const bootstrapjs = gulp.src('node_modules/bootstrap/dist/js/*.js')
         .pipe( gulp.dest('public/assets/js/') );
 
-    var jquery = gulp.src('node_modules/jquery/dist/*.js')
+    const jquery = gulp.src('node_modules/jquery/dist/*.js')
         .pipe( gulp.dest('public/assets/js/') );
 
     return merge( bootstrap,bootstrapjs,jquery );
 
+});
+
+
+gulp.task('sass', function() {
+
+    const sasstranspilation = gulp.src('./assets/scss/general.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('public/assets/css/'));
+
+    const minify = gulp.src('public/assets/css/general.css')
+          .pipe( gulpminify() )
+          .pipe( gulp.dest('public/assets/css/'));
+
+    return merge( sasstranspilation,minify );
+
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
