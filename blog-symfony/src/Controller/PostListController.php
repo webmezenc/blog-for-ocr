ed8bfa66a9a1f38f2b367c8b888f1aa0596a0b00
webmezenc <contact@webmezenc.com>
2018-08-02 18:33:23 +0200

    Finalisation de la feature

--- blog-symfony/src/Controller/PostListController.php
+++ blog-symfony/src/Controller/PostListController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Utils\Services\Post\PostServices;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -14,16 +15,41 @@ class PostListController extends AppController
      */
     public function index()
     {
+       return $this -> createPagePostList(0,10);
+    }
+
+
+    /**
+     * @Route("/post/list/{pageNumber}", name="post_list_page")
+     */
+    public function pageList( int $pageNumber )
+    {
+        $start = $pageNumber * 10;
+
+        return $this -> createPagePostList( $start,10, $pageNumber );
+    }
+
+
+    /**
+     * @param int $start
+     * @param int $numberPost
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    private function createPagePostList( int $start, int $numberPost, int $actualPage = 0 ): Response
+    {
 
         $PostRepository = $this -> getDoctrine() -> getRepository(Post::class);
         $PostServices = new PostServices( $PostRepository );
 
         try {
 
-            $postList = $PostServices -> getListPost(0,10 );
+            $postList = $PostServices -> getListPost($start,$numberPost );
 
             return $this->render('page/post-list.html.twig', [
                 'postList' => $postList,
+                'postNumber' => $PostRepository -> getNumberOfValidPost(),
+                'actualPage' => $actualPage
             ]);
 
         } catch( \Exception $e ) {
@@ -31,4 +57,6 @@ class PostListController extends AppController
         }
 
     }
+
+
 }
